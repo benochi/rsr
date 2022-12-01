fdcf33fd7fb2e83855c51f73b3bd9e43bcb8a32e
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-12-01 17:55:22 -0600

    Boom

--- src/components/Dictaphone.js
+++ src/components/Dictaphone.js
@@ -1,64 +1,70 @@
 import React, { useEffect, useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
-const Dictaphone = () => {
-  const {
-    transcript,
-    interimTranscript,
-    finalTranscript,
-    resetTranscript,
-    listening,
-    } = useSpeechRecognition({ commands });
-  
-  useEffect(() => {
-    if (finalTranscript !== '') {
-      console.log('Got final result:', finalTranscript);
-    }
-  }, [interimTranscript, finalTranscript]);
+const Dictaphone1 = () => {
+ const [message, setMessage] = useState('');
+ const commands = [
+   {
+     command: 'reset',
+     callback: () => resetTranscript()
+   },
+   {
+     command: 'shut up',
+     callback: () => setMessage('I wasn\'t talking.')
+   },
+   {
+     command: 'Hello',
+     callback: () => setMessage('Hi there!')
+   },
+ ]
+ const {
+   transcript,
+   interimTranscript,
+   finalTranscript,
+   resetTranscript,
+   listening,
+ } = useSpeechRecognition({ commands });
 
-  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
-    return null;
-  }
- 
-  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
-    console.log('Your browser does not support speech recognition software! Try Chrome desktop, maybe?');
-  }
-  const listenContinuously = () => {
-    SpeechRecognition.startListening({
-      continuous: true,
-      language: 'en-GB',
-    });
-  };
-  
-  const commands = [
-    {
-      command: 'reset',
-      callback: () => resetTranscript()
-    },
-    {
-      command: 'clear',
-      callback: () => resetTranscript()
-    }
-  ]
-  return (
-    <div>
-      <div>
-        <span>
-          listening:
-          {' '}
-          {listening ? 'on' : 'off'}
-        </span>
-        <div>
-          <button type="button" onClick={resetTranscript}>Reset</button>
-          <button type="button" onClick={listenContinuously}>Listen</button>
-          <button type="button" onClick={SpeechRecognition.stopListening}>Stop</button>
-        </div>
-      </div>
-      <div>
-        <span>{transcript}</span>
-      </div>
-    </div>
-  );
+ useEffect(() => {
+   if (finalTranscript !== '') {
+     console.log('Got final result:', finalTranscript);
+   }
+ }, [interimTranscript, finalTranscript]);
+ if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+   return null;
+ }
+
+ if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+   console.log('Your browser does not support speech recognition software! Try Chrome desktop, maybe?');
+ }
+ const listenContinuously = () => {
+   SpeechRecognition.startListening({
+     continuous: true,
+     language: 'en-GB',
+   });
  };
- 
- export default Dictaphone;
+ return (
+   <div>
+     <div>
+       <span>
+         listening:
+         {' '}
+         {listening ? 'on' : 'off'}
+       </span>
+       <div>
+         <button type="button" onClick={resetTranscript}>Reset</button>
+         <button type="button" onClick={listenContinuously}>Listen</button>
+         <button type="button" onClick={SpeechRecognition.stopListening}>Stop</button>
+       </div>
+     </div>
+     <div>
+       {message}
+     </div>
+     <div>
+       <span>{transcript}</span>
+     </div>
+   </div>
+ );
+};
+
+export default Dictaphone1;
